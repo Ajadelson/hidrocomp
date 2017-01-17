c7517c923f53b95adbcbc17b2f04d0b6ca90f8c7
Ewerton Amorim <ewerton.amorim@live.com>
2017-01-16 22:22:36 -0300

    Commit Inicial - adicionado os models e admins.

---
+++ data/admin.py
@@ -0,0 +1,76 @@
+from django.contrib import admin
+from .models import Discretizacao,Reducao,Posto,Fonte,TipoPosto,Localizacao,Unidade,Variavel,NivelConsistencia,SerieReduzida,SerieOriginal
+
+class TipoPostoAdmin(admin.ModelAdmin):
+    model = TipoPosto
+    list_display = ['tipo']
+    search_fields = ['tipo']
+    save_on_top = True
+class FonteAdmin(admin.ModelAdmin):
+    model = Fonte
+    list_display = ['tipo']
+    search_fields = ['tipo']
+    save_on_top = True
+class LocalizacaoAdmin(admin.ModelAdmin):
+    model = Localizacao
+    list_display = ['coordenadas']
+    #search_fields = ['tipo']
+    save_on_top = True
+class PostoAdmin(admin.ModelAdmin):
+    model = Posto
+    list_display = ['nome','tipo_posto','fonte','codigo_ana','localizacao']
+    search_fields = ['nome']
+    save_on_top = True
+
+class UnidadeAdmin(admin.ModelAdmin):
+    model = Unidade
+    list_display = ['tipo']
+    search_fields = ['tipo']
+    save_on_top = True
+class VariavelAdmin(admin.ModelAdmin):
+    model = Variavel
+    list_display = ['variavel']
+    search_fields = ['variavel']
+    save_on_top = True
+class NivelConsistenciaAdmin(admin.ModelAdmin):
+    model = NivelConsistencia
+    list_display = ['tipo']
+    search_fields = ['tipo']
+    save_on_top = True
+class DiscretizacaoAdmin(admin.ModelAdmin):
+    model = Discretizacao
+    list_display = ['tipo']
+    search_fields = ['tipo']
+    save_on_top = True
+class ReducaoAdmin(admin.ModelAdmin):
+    model = Reducao
+    list_display = ['tipo']
+    search_fields = ['tipo']
+    save_on_top = True
+
+class SerieReduzidaAdmin(admin.ModelAdmin):
+    model = SerieReduzida
+    list_display = ['serie_original','discretizacao','reducao','serie_temporal_id']
+    search_fields = ['serie_original__id']
+    save_on_top = True
+
+class SerieOriginalAdmin(admin.ModelAdmin):
+    model = SerieOriginal
+    #list_display = ['Discretizacao_','Unidade_','Variavel_','Tipo_Dado_','serie_Temporal_id']
+    list_display = ['serie_temporal_id','posto','discretizacao','variavel','unidade','tipo_dado']
+    search_fields = ['serie_Temporal_id']
+    save_on_top = True
+
+
+
+admin.site.register(TipoPosto,TipoPostoAdmin)
+admin.site.register(Fonte,FonteAdmin)
+admin.site.register(Localizacao,LocalizacaoAdmin)
+admin.site.register(Posto,PostoAdmin)
+admin.site.register(Unidade,UnidadeAdmin)
+admin.site.register(Variavel,VariavelAdmin)
+admin.site.register(NivelConsistencia,NivelConsistenciaAdmin)
+admin.site.register(Discretizacao,DiscretizacaoAdmin)
+admin.site.register(Reducao,ReducaoAdmin)
+admin.site.register(SerieOriginal,SerieOriginalAdmin)
+admin.site.register(SerieReduzida,SerieReduzidaAdmin)
---
+++ data/models.py
@@ -0,0 +1,124 @@
+from django.contrib.gis.db import models
+
+
+class Localizacao(models.Model):
+    coordenadas = models.PointField(srid=4326)
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return '%s %s' % (self.coordenadas.x, self.coordenadas.y)
+    
+    
+    
+    
+from django.utils.translation import ugettext_lazy as _
+
+'''#################### **************** SÉRIE TEMPORAL *************** #####################'''
+
+class SerieTemporal(models.Model):
+    Id = models.IntegerField(unique=False)
+    dado = models.FloatField()
+    data_e_hora = models.DateTimeField(verbose_name = 'Data e Hora',unique=False)
+    class Meta:
+        unique_together = (("Id","data_e_hora"),)
+        verbose_name_plural = "Séries Temporais"
+        verbose_name = "Série Temporal"
+        
+#ids = [n.Id for n in SerieTemporal.objects.all()]
+#choices = ((str(n),str(n)) for n in set(ids))
+
+'''#################### **************** POSTO *************** #####################'''
+class TipoPosto(models.Model):
+    tipo = models.CharField(max_length=20)
+    class Meta:
+        verbose_name_plural = "Tipos de Posto"
+        verbose_name = "Tipo de Posto"
+    def __str__(self):
+        return '%s'%self.tipo
+class Fonte(models.Model):
+    tipo = models.CharField(max_length=15)
+    def __str__(self):
+        return '%s'%self.tipo
+
+class Posto(models.Model):
+    tipo_posto = models.ForeignKey(TipoPosto)
+    fonte = models.ForeignKey(Fonte)
+    codigo_ana = models.CharField(max_length=15,null=True)
+    nome = models.CharField(max_length=30)
+    localizacao = models.ForeignKey(Localizacao)
+    def __str__(self):
+        return '%s %s (%s)'%(self.nome,self.fonte.tipo,self.codigo_ana)
+
+'''#################### **************** PRÉ-REQUISITOS PARA SÉRIE ORIGINAL E REDUZIDA *************** #####################'''
+class Discretizacao(models.Model):
+    tipo = models.CharField(max_length=20)
+    class Meta:
+        verbose_name_plural = "Discretizações"
+        verbose_name = "Discretização"
+    def __str__(self):
+        return '%s'%self.tipo
+    
+    
+class Unidade(models.Model):
+    tipo = models.CharField(max_length=20)
+    class Meta:
+        verbose_name_plural = "Unidades de Medida"
+        verbose_name = "Unidade de Medida"
+    def __str__(self):
+        return '%s'%self.tipo
+    
+    
+class Variavel(models.Model):
+    variavel = models.CharField(max_length=20)
+    class Meta:
+        verbose_name_plural = "Variáveis"
+        verbose_name = "Variável"
+    def __str__(self):
+        return '%s'%self.variavel
+class NivelConsistencia(models.Model):
+    tipo = models.CharField(max_length=20)
+    class Meta:
+        verbose_name_plural = "Níveis de Consistência"
+        verbose_name = "Nível de Consistência"
+    def __str__(self):
+        return '%s'%self.tipo
+class Reducao(models.Model):
+    tipo = models.CharField(max_length=20)
+    class Meta:
+        verbose_name_plural = "Reduções"
+        verbose_name = "Redução"
+    def __str__(self):
+        return '%s'%self.tipo
+'''#################### **************** SÉRIE ORIGINAL *************** #####################'''
+
+class SerieOriginal(models.Model):
+    posto = models.ForeignKey(Posto)
+    arquivo_Fonte_data = models.DateTimeField(auto_now_add=True,verbose_name = 'Data e Hora',unique=False)
+    variavel = models.ForeignKey(Variavel)
+    tipo_dado = models.ForeignKey(NivelConsistencia)
+    discretizacao = models.ForeignKey(Discretizacao)
+    unidade = models.ForeignKey(Unidade)
+    choices = (('ok','teste'),)
+    
+    serie_temporal_id = models.CharField(max_length=1,choices=choices)
+    def __str__(self):
+        return 'Série %s do posto de %s, com dados de %s (%s)'%(self.discretizacao.tipo,self.posto,self.variavel,self.unidade)
+    
+    class Meta:
+        verbose_name_plural = "Séries Originais"
+        verbose_name = "Série Original"
+
+'''#################### **************** SÉRIE REDUZIDA *************** #####################'''
+
+
+class SerieReduzida(models.Model):
+    serie_original = models.ForeignKey(SerieOriginal)
+    discretizacao = models.ForeignKey(Discretizacao)
+    reducao = models.ForeignKey(Reducao)
+    choices = (('ok','teste'),)
+    serie_temporal_id = models.CharField(max_length=1,choices=choices)
+    class Meta:
+        verbose_name_plural = "Séries Reduzidas"
+        verbose_name = "Série Reduzida"
+
+
